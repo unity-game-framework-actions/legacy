243ca715e2c7a1e76b32c80dd591127c2c5fec67
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-03-08 15:42:00 +0200

    Format code

--- changelog/src/changelog.ts
+++ changelog/src/changelog.ts
@@ -1,267 +1,279 @@
-
 export type ChangelogConfig = {
-    repoUrl: string
-    header: string
-    description: string
-    sections: SectionConfig[]
-    footer: string
+  repoUrl: string
+  header: string
+  description: string
+  sections: SectionConfig[]
+  footer: string
 }
 
 export type SectionConfig = {
-    name: string
-    labels: string[]
+  name: string
+  labels: string[]
 }
 
 type Changelog = {
-    milestones: ChangelogMilestone[]
+  milestones: ChangelogMilestone[]
 }
 
 type ChangelogMilestone = {
-    name: string
-    number: number
-    date: Date
-    sections: ChangelogSection[]
+  name: string
+  number: number
+  date: Date
+  sections: ChangelogSection[]
 }
 
 type ChangelogSection = {
-    name: string
-    issues: IssueInfo[]
+  name: string
+  issues: IssueInfo[]
 }
 
 type MilestoneGroupInfo = {
-    milestone: MilestoneInfo
-    issues: IssueInfo[]
+  milestone: MilestoneInfo
+  issues: IssueInfo[]
 }
 
 type IssueInfo = {
-    number: number
-    title: string
-    body: string
-    labels: LabelInfo[]
-    milestone: MilestoneInfo
-    pull_request: PullRequestInfo
+  number: number
+  title: string
+  body: string
+  labels: LabelInfo[]
+  milestone: MilestoneInfo
+  pull_request: PullRequestInfo
 }
 
 type PullRequestInfo = {
-    url: string
+  url: string
 }
 
 type MilestoneInfo = {
-    number: number
-    title: string
-    description: string
-    closed_at: string
+  number: number
+  title: string
+  description: string
+  closed_at: string
 }
 
 type LabelInfo = {
-    name: string
+  name: string
 }
 
 export function createDefaultConfig(repoUrl: string): ChangelogConfig {
-    return {
-        repoUrl: repoUrl,
-        header: 'Changelog',
-        description: `All notable changes to this project will be documented in this file.
+  return {
+    repoUrl: repoUrl,
+    header: 'Changelog',
+    description: `All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).`,
-        footer: `---
+    footer: `---
 > Unity Game Framework | Copyright 2019`,
-        sections: [
-            {
-                name: 'Added',
-                labels: ['Added']
-            },
-            {
-                name: 'Changed',
-                labels: ['Changed']
-            },
-            {
-                name: 'Deprecated',
-                labels: ['Deprecated']
-            },
-            {
-                name: 'Removed',
-                labels: ['Removed']
-            },
-            {
-                name: 'Fixed',
-                labels: ['Fixed']
-            },
-            {
-                name: 'Security',
-                labels: ['Security']
-            }
-        ]
-    }
+    sections: [
+      {
+        name: 'Added',
+        labels: ['Added']
+      },
+      {
+        name: 'Changed',
+        labels: ['Changed']
+      },
+      {
+        name: 'Deprecated',
+        labels: ['Deprecated']
+      },
+      {
+        name: 'Removed',
+        labels: ['Removed']
+      },
+      {
+        name: 'Fixed',
+        labels: ['Fixed']
+      },
+      {
+        name: 'Security',
+        labels: ['Security']
+      }
+    ]
+  }
 }
 
-export function formatChangelog(changelog: Changelog, config: ChangelogConfig): string {
-    let format = ''
-    const lineEnd = '\n\r'
+export function formatChangelog(
+  changelog: Changelog,
+  config: ChangelogConfig
+): string {
+  let format = ''
 
-    format += '# ' + config.header + lineEnd
-    format += config.description + lineEnd
+  format += `# ${config.header}`
+  format += `\n\r${config.description}`
 
-    for (const milestone of changelog.milestones) {
-        format += formatMilestone(milestone, config)
-    }
+  for (const milestone of changelog.milestones) {
+    format += formatMilestone(milestone, config)
+  }
 
-    format += config.footer + lineEnd
+  format += `\n\r${config.footer}`
 
-    return format
+  return format
 }
 
-function formatMilestone(milestone: ChangelogMilestone, config: ChangelogConfig): string {
-    let format = ''
-    const lineEnd = '\n\r'
+function formatMilestone(
+  milestone: ChangelogMilestone,
+  config: ChangelogConfig
+): string {
+  let format = ''
 
-    format += '## ' + milestone.name + ' - ' + milestone.date.toISOString() + lineEnd
-    format += ' - [Commits](' + config.repoUrl + '/compare/0...' + milestone.name + ')' + lineEnd
-    format += ' - [Milestone](' + config.repoUrl + '/milestone/' + milestone.number + '?closed=1)' + lineEnd
+  format += `\n\r## ${milestone.name} - ${milestone.date.toUTCString()}`
+  format += `\n\r - [Commits](${config.repoUrl}/compare/0...${milestone.name})`
+  format += `\n\r - [Milestone](${config.repoUrl}/milestone/${milestone.number}?closed=1)`
 
-    for (const section of milestone.sections) {
-        format += formatSection(section)    
-    }
+  for (const section of milestone.sections) {
+    format += formatSection(section)
+  }
 
-    format += lineEnd
-
-    return format
+  return format
 }
 
 function formatSection(section: ChangelogSection): string {
-    let format = ''
-    const lineEnd = '\n\r'
+  let format = ''
 
-    format += '### ' + section.name + lineEnd
+  format += `\n\r### ${section.name}`
 
-    for (const issue of section.issues) {
-        format += formatIssue(issue)
-    }
+  for (const issue of section.issues) {
+    format += formatIssue(issue)
+  }
 
-    format += lineEnd
-
-    return format
+  return format
 }
 
 function formatIssue(issue: IssueInfo): string {
-    let format = ''
-    const lineEnd = '\n\r'
+  let format = ''
 
-    format += ' - ' + issue.title + ' (#' + issue.number + ')'
-    format += lineEnd
+  format += `\n\r - ${issue.title} (#${issue.number})`
 
-    return format
+  return format
 }
 
-export function createChangelog(milestones: MilestoneInfo[], issues: IssueInfo[], config: ChangelogConfig): Changelog {
-    const changelog: Changelog = {
-        milestones: []
-    }
+export function createChangelog(
+  milestones: MilestoneInfo[],
+  issues: IssueInfo[],
+  config: ChangelogConfig
+): Changelog {
+  const changelog: Changelog = {
+    milestones: []
+  }
 
-    const groups = getMilestoneGroups(milestones, issues)
+  const groups = getMilestoneGroups(milestones, issues)
 
-    for (const group of groups) {
-        const milestone = createMilestone(group, config.sections)
+  for (const group of groups) {
+    const milestone = createMilestone(group, config.sections)
 
-        changelog.milestones.push(milestone)
-    }
+    changelog.milestones.push(milestone)
+  }
 
-    changelog.milestones.sort((a, b) => a.date.getTime() - b.date.getTime())
+  changelog.milestones.sort((a, b) => a.date.getTime() - b.date.getTime())
 
-    return changelog
+  return changelog
 }
 
-function createMilestone(milestoneGroup: MilestoneGroupInfo, sectionConfigs: SectionConfig[]): ChangelogMilestone {
-    const milestone: ChangelogMilestone = {
-        name: milestoneGroup.milestone.title,
-        number: milestoneGroup.milestone.number,
-        date: new Date(milestoneGroup.milestone.closed_at),
-        sections: []
+function createMilestone(
+  milestoneGroup: MilestoneGroupInfo,
+  sectionConfigs: SectionConfig[]
+): ChangelogMilestone {
+  const milestone: ChangelogMilestone = {
+    name: milestoneGroup.milestone.title,
+    number: milestoneGroup.milestone.number,
+    date: new Date(milestoneGroup.milestone.closed_at),
+    sections: []
+  }
+
+  const issues = groupIssuesBySection(milestoneGroup.issues, sectionConfigs)
+
+  issues.forEach((value, key) => {
+    const section: ChangelogSection = {
+      name: key,
+      issues: value
     }
 
-    const issues = groupIssuesBySection(milestoneGroup.issues, sectionConfigs)
+    section.issues.sort((a, b) => a.title.localeCompare(b.title))
 
-    issues.forEach((value, key) => {
-        const section: ChangelogSection = {
-            name: key,
-            issues: value
-        }
+    milestone.sections.push(section)
+  })
 
-        section.issues.sort((a, b) => a.title.localeCompare(b.title))
+  milestone.sections.sort((a, b) => a.name.localeCompare(b.name))
 
-        milestone.sections.push(section)
-    })
-
-    milestone.sections.sort((a, b) => a.name.localeCompare(b.name))
-
-    return milestone
+  return milestone
 }
 
-function getMilestoneGroups(milestones: MilestoneInfo[], issues: IssueInfo[]): MilestoneGroupInfo[] {
-    var groups = new Map<number, MilestoneGroupInfo>()
+function getMilestoneGroups(
+  milestones: MilestoneInfo[],
+  issues: IssueInfo[]
+): MilestoneGroupInfo[] {
+  var groups = new Map<number, MilestoneGroupInfo>()
 
-    for (const milestone of milestones) {
-        const info: MilestoneGroupInfo = {
-            milestone: milestone,
-            issues: []
-        }
-
-        groups.set(milestone.number, info)
+  for (const milestone of milestones) {
+    const info: MilestoneGroupInfo = {
+      milestone: milestone,
+      issues: []
     }
 
-    for (const issue of issues) {
-        if (issue.pull_request == null && issue.milestone != null) {
-            const info = groups.get(issue.milestone.number)
+    groups.set(milestone.number, info)
+  }
 
-            info?.issues.push(issue)
-        }
+  for (const issue of issues) {
+    if (issue.pull_request == null && issue.milestone != null) {
+      const info = groups.get(issue.milestone.number)
+
+      info?.issues.push(issue)
     }
+  }
 
-    return Array.from(groups.values())
+  return Array.from(groups.values())
 }
 
-function groupIssuesBySection(issues: IssueInfo[], sectionConfigs: SectionConfig[]): Map<string, IssueInfo[]> {
-    const groups = new Map<string, IssueInfo[]>()
+function groupIssuesBySection(
+  issues: IssueInfo[],
+  sectionConfigs: SectionConfig[]
+): Map<string, IssueInfo[]> {
+  const groups = new Map<string, IssueInfo[]>()
 
-    for (const issue of issues) {
-        for (const section of sectionConfigs) {
-            if (hasAnyLabel(issue.labels, section.labels)) {
-                const group = getOrCreate(groups, section.name)
+  for (const issue of issues) {
+    for (const section of sectionConfigs) {
+      if (hasAnyLabel(issue.labels, section.labels)) {
+        const group = getOrCreate(groups, section.name)
 
-                group.push(issue)
-            }
-        }
+        group.push(issue)
+      }
     }
+  }
 
-    return groups
+  return groups
 }
 
-function getOrCreate<TKey, TArray>(map: Map<TKey, TArray[]>, key: TKey): TArray[] {
-    let result: TArray[] | undefined = map.get(key)
+function getOrCreate<TKey, TArray>(
+  map: Map<TKey, TArray[]>,
+  key: TKey
+): TArray[] {
+  let result: TArray[] | undefined = map.get(key)
 
-    if (result == undefined) {
-        result = []
-        map.set(key, result)            
-    }
+  if (result == undefined) {
+    result = []
+    map.set(key, result)
+  }
 
-    return result
+  return result
 }
 
 function hasAnyLabel(labels: LabelInfo[], names: string[]): boolean {
-    for (const name of names) {
-        if (hasLabel(labels, name)) {
-            return true
-        }
+  for (const name of names) {
+    if (hasLabel(labels, name)) {
+      return true
     }
-    return false
+  }
+  return false
 }
 
 function hasLabel(labels: LabelInfo[], name: string): boolean {
-    for (const label of labels) {
-        if (label.name.toLowerCase === name.toLowerCase) {
-            return true
-        }
+  for (const label of labels) {
+    if (label.name.toLowerCase === name.toLowerCase) {
+      return true
     }
-    return false
+  }
+  return false
 }
--- changelog/src/main.ts
+++ changelog/src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import { context, GitHub } from "@actions/github"
+import {context, GitHub} from '@actions/github'
 import * as changelog from './changelog'
 
 run()
@@ -11,12 +11,16 @@ async function run(): Promise<void> {
     const owner = context.repo.owner
     const repo = context.repo.repo
     const url = 'https://github.com/' + owner + '/' + repo
-    const milestones = (await github.issues.listMilestonesForRepo({ owner, repo, state: 'all' })).data
-    const issues = (await github.issues.listForRepo({ owner, repo, state: 'all' })).data
+    const milestones = (
+      await github.issues.listMilestonesForRepo({owner, repo, state: 'all'})
+    ).data
+    const issues = (
+      await github.issues.listForRepo({owner, repo, state: 'all'})
+    ).data
     const config = changelog.createDefaultConfig(url)
     const log = changelog.createChangelog(milestones, issues, config)
     const format = changelog.formatChangelog(log, config)
-    
+
     console.log(format)
     console.log(JSON.stringify(log, null, 2))
     console.log(JSON.stringify(milestones, null, 2))
